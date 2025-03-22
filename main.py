708ac94b2e1fd367b63c9ff4c6ed392e3bbedd21
stalker121 <miha00lojb@gmail.com>
2025-03-22 13:27:29 +0200

    added

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ class LikeApp(App):
     def build(self):
         self.mainBox = BoxLayout(orientation="vertical")
         btnBox=BoxLayout()
-        lbl_title=Label(text="Rate the photo",font_size=32,halign="center",size_hint=[1,0.1])
-        btn_like=Button(text="Like",font_size=24,size_hint=[0.5,0.3])
-        btn_dislike=Button(text="Dislike",font_size=24,size_hint=[0.5,0.3])
-        self.img=Image(source="photo/images.jpg",size_hint=[1,0.6])
+        lbl_title=Label(text="оцени-ка фоточку",font_size=32,halign="center",size_hint=[1,0.1])
+        btn_like=Button(text="крута",font_size=24,size_hint=[0.5,0.3],on_press=self.like)
+        btn_dislike=Button(text="фигня",font_size=24,size_hint=[0.5,0.3],on_press=self.dislike)
+        self.img=Image(source="photo/images.jpg")
         self.mainBox.add_widget(lbl_title)
         self.mainBox.add_widget(self.img)
         btnBox.add_widget(btn_like)
         btnBox.add_widget(btn_dislike)
         self.mainBox.add_widget(btnBox)
         return self.mainBox
+    def like(self,btn):
+        print("Крута!")
+    def dislike(self,btn):
+        print("Фигня!")
 LikeApp().run()
